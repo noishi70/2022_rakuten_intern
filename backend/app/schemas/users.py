361b7eb9ae37b03e6c0ef6ddefb9af48e006a5e9
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-13 14:08:14 +0900

    feat: body格納用のスキーマ

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -5,10 +5,12 @@ from pydantic import BaseModel, Field
 
 class User(BaseModel):
     user_id: int 
-    name: str
+    name: Optional[str] = Field(None)
     header_img: Optional[str] = Field(None)
     icon: Optional[str] = Field(None)
     comment: Optional[str] = Field(None)
+    email: str
+    password: str
 
     class Config:
         orm_mode = True
