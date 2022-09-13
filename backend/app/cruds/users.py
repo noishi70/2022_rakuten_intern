a60db919b5c6161040d16f5eb78e03c9dcf8bed1
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-13 15:31:02 +0900

    feat: update_user

--- backend/app/cruds/users.py
+++ backend/app/cruds/users.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from db import session
 from models.models import User
 
@@ -11,6 +13,31 @@ def signup(email: str, hashed_password: str) -> None:
     user = User(name="", email=email, hashed_password=hashed_password)
     session.add(user)
     session.commit()
+    session.close()
 
+    return
+
+
+def update_me(
+    user_id: int,
+    name: Optional[str] = "", 
+    header_img: Optional[str] = None,
+    icon: Optional[str] = None, 
+    comment: Optional[str] = None
+    ) -> None:
+    
+    me = session.query(User).filter(User.user_id==user_id).first()
+    if name:
+        me.name = name
+    if header_img:
+        me.header_img = header_img
+    if icon:
+        me.icon = icon
+    if comment:
+        me.comment = comment
+    
+    session.commit()
+    session.close()
     return 
+     
     
