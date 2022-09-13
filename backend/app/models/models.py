7d434552565b1c9ba87d5aee210bb4b112d0ac79
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-13 11:41:47 +0900

    chore: users -> modelsに名前変更

---
+++ backend/app/models/models.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+from db import Base
+
+
+class UserFavoritePost(Base):
+    """User がお気に入りに登録した Post
+    """
+    __tablename__ = "user_favorite_posts"
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32), nullable=False)
+    header_img = Column(String(1024))
+    icon = Column(String(1024))
+    comment = Column(String(512))
+    hashed_passowrd = Column(String(512), nullable=False)
+
+    post = relationship("Post", back_populates="users")
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), nullable=False)
+    content = Column(String(1024))
+    url = Column(String(512))
+    datetime = Column(DateTime, default=datetime.now().time(), nullable=False)
+    duration = Column(Integer)
+    created_by = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+
+# class ProvisionalUser(Base):
+#     __tablename__ = "provisional_users"
+
+class Follow(Base):
+    __tablename__ = "follow"
+
+    follower_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    followee_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+
