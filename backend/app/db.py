b251c67bf636becad0bffdab7839009e97129ea5
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-13 10:12:52 +0900

    refac: async -> sync engine

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,15 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
+URL = "mysql+pymysql://root:docker@db:3306/demo?charset=utf8"
 
-async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
-async_session = sessionmaker(
-    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+Engine = create_engine(URL, echo=True)
+session = sessionmaker(
+    autocommit=False, autoflush=False, bind=Engine
 )
 
 Base = declarative_base()
-
-async def get_db():
-    async with async_session() as session:
-        yield session
