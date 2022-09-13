d7a68ec5e3fad8d6b7fecba6fb34a535ade3fa13
tokun4869 <ba.tokun4869@gmail.com>
2022-09-13 23:04:32 +0900

    Contentコンポーネントの一時完成

--- front/src/components/Content.tsx
+++ front/src/components/Content.tsx
@@ -12,28 +12,48 @@ type Props = {
   url: string
   time: number
   datetime: string
+  setValue?: (page: string) => void;
 }
 
 const Content = (props:Props) => {
   const [open, setOpen] = useState(false);
+  const text = props.content;
+  const minText = text.substring(0, 21);
 
   return (
     <div className={Style.content}>
-      <div>
-        <p>{props.name}</p>
-        <img src={'data:image/png;base64,' + props.icon} alt='icon' />
-        <p>{props.title}</p>
-        
-        <div className={ open ? Style.accordionopen : Style.accordion }>
-          {/*{ open ? <MultiLine line={props.content} /> : <SmallLine line={props.content} /> }*/}
-          <MultiLine line={props.content} />
-          <a href={props.url}>{props.url}</a>
-        </div>
-        { open ? <button className={Style.openbutton} onClick={() => setOpen(false)}>表示を減らす</button> : <button className={Style.openbutton} onClick={() => setOpen(true)}>続きを表示</button> }
-        
-      </div>
-      <Grid>
-      
+      <Grid container spacing={1}>
+        <Grid item xs={3}>
+          <Grid style={{ height: "100%" }}>
+            <img src={'data:image/png;base64,' + props.icon} alt='icon' className={Style.img} onClick={() => props.setValue?.('MyProfile')}/>
+          </Grid>
+        </Grid>
+        <Grid item xs={9}>
+          <Grid container spacing={1}>
+            <Grid item xs={6} className={Style.user_info}>
+              <p className={Style.name}>{props.name}</p>
+              <p className={Style.user_id}>@{props.user_id}</p>
+            </Grid>
+            <Grid item xs={6} className={Style.datetime}>
+              <p>{props.datetime}</p>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <p className={Style.title}>{props.title}</p>
+            <Grid item xs={12} className={ text.length > 21 ? ( open ? Style.accordionopen : Style.accordion ) : null }>
+              <MultiLine line={ text.length > 21 || minText.indexOf('\n') != -1 ? ( open ? text : ( minText.indexOf('\n') == -1 ? minText + '...' : minText.split('\n')[0] + '...' )) : minText }/>
+              <Grid item xs={1}>
+                { text.length > 21 || minText.indexOf('\n') != -1 ? ( open ? <a href={props.url} className={Style.url}>{props.url}</a> : null ) : <a href={props.url} className={Style.url}>{props.url}</a> }
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              { text.length > 21
+                ? ( open ? <button className={Style.openbutton} onClick={() => setOpen(false)}>表示を減らす</button> : <button className={Style.openbutton} onClick={() => setOpen(true)}>続きを表示</button> )
+                : null 
+              }
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
@@ -45,23 +65,12 @@ const MultiLine = ({ line }: { line: string }) => {
   const lines = line.split('\n').map((item, index) => {
     return (
       <React.Fragment key={index}>
-        {item}<br />
+        <p>{item}</p>
       </React.Fragment>
     );
   });
 
-  return <p>{lines}</p>
-}
-
-const SmallLine = ({ line }: { line: string }) => {
-  const small = line.substring(0, 10);
-  const lines = small.split('\n').map((item, index) => {
-    return (
-      <React.Fragment key={index}>
-        {item}<br />
-      </React.Fragment>
-    );
-  });
-
-  return <p>{lines}</p>
+  return (
+    <div className={Style.text}>{lines}</div>
+  );
 }
