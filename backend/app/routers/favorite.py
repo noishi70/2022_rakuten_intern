6f1d4f46852bc2e48b40e1e9413d0a22b8874469
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-14 16:28:48 +0900

    refac: schemaに合わせる

--- backend/app/routers/favorite.py
+++ backend/app/routers/favorite.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter, Depends
-from typing import List
 
 from libs.auth import get_current_user
 from schemas.users import User
 from schemas.posts import Post as schemas_post
 from models.models import Post as models_post
+from cruds.users import get_user_by_id
 from cruds.favorite import favorite_post_lists, give_favorite, del_favorite
 
 
 router = APIRouter(tags=["favorite"])
 
-@router.get("/users/favorite", response_model=List[schemas_post])
+@router.get("/users/favorite", response_model=list[schemas_post])
 async def fetch_favorite_posts(current_user: User = Depends(get_current_user)):
     posts = favorite_post_lists(current_user.user_id)
-    res_posts = [schemas_post(**post.to_dict()) for post in posts]
+
+    res_posts = [schemas_post(**post.to_dict(), name=post.user_id, icon=get_user_by_id(post.user_id).icon.v) for post in posts]
     return res_posts
 
 
