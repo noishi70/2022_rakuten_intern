61a8bd456042e7f95e7c367b095a388ca6c3a0ea
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-14 17:10:52 +0900

    refac: エンドポイントの読み込み変更

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ from libs import auth
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(favorite.router, prefix="/api")
 app.include_router(users.router, prefix="/api/users")
 app.include_router(posts.router, prefix="/api")
 app.include_router(follow.router, prefix="/api")
-app.include_router(favorite.router, prefix="/api")
--- backend/app/routers/favorite.py
+++ backend/app/routers/favorite.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter, Depends
 from libs.auth import get_current_user
 from schemas.users import User
 from schemas.posts import Post as schemas_post
-from models.models import Post as models_post
 from cruds.users import get_user_by_id
 from cruds.favorite import favorite_post_lists, give_favorite, del_favorite
 
@@ -13,9 +12,10 @@ router = APIRouter(tags=["favorite"])
 @router.get("/users/favorite", response_model=list[schemas_post])
 async def fetch_favorite_posts(current_user: User = Depends(get_current_user)):
     posts = favorite_post_lists(user_id=current_user.user_id)
-
-    res_posts = [schemas_post(**post.to_dict(), name=post.user_id.v, icon=get_user_by_id(post.user_id).icon.v) for post in posts]
-    return res_posts
+    if posts:
+        res_posts = [schemas_post(**post.to_dict(), name=post.user_id, icon=get_user_by_id(post.user_id).icon) for post in posts]
+        return res_posts
+    
 
 
 @router.post("/users/favorite")
