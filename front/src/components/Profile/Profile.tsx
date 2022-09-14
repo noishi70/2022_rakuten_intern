b34cfa732a002f4e6f18c959887acc44ba80642b
noishi70 <74105563+noishi70@users.noreply.github.com>
2022-09-14 10:43:18 +0900

    Element placement complete

---
+++ front/src/components/Profile/Profile.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import HeadImg from "../../back.jpg";
+import "./Profile.css";
+
+const Profile = () => {
+  return (
+    <>
+      <div className="head">
+        <img src={HeadImg} className="headimg" alt="Header" />
+      </div>
+      
+      <div className="row">
+        <div className="iconname">
+          <img src={HeadImg} className="iconimage" alt="icon" />
+          <div className="username">UserName</div>
+        </div>
+        <button className="profilebutton">プロフィール編集</button>
+      </div>
+      
+
+      <div className="profile">
+        <p className="profileintro">
+          fdaffffffffffffffssssssssssssssssssssssssffffffffffafdsafsadfasf
+        </p>
+      </div>
+
+      <div className="followfollower">
+        <div className="follow">1234 フォロー</div>
+        <div className="follower">5678 フォロワー</div>
+
+      </div>
+
+      <hr></hr>
+    </>
+  );
+};
+
+export default Profile;
