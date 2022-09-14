129daa0650c01fb759cf82dc0bc17170e4888b70
noishi70 <74105563+noishi70@users.noreply.github.com>
2022-09-14 15:05:41 +0900

    chore

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -4,12 +4,20 @@ import Search from '../../footerImg/search.svg'
 import Post from '../../footerImg/post.svg'
 import './Footer.css'
 
-const Footer = () => {
+type props = {
+    setMode?: (mode: string) => void;
+
+}
+
+const Footer = (props:props) => {
     return (
         <div className="footer">
-        <img src={Home} alt="home"  className="icon"/>
+            <button onClick={() => props.setMode?.('Home')} className="iconbutton"><img src={Home} alt="home"  className="icon"/></button>
+            <button onClick={() => props.setMode?.('Search')} className="iconbutton"><img src={Search} alt="search"  className="icon"/></button>
+            <button onClick={() => props.setMode?.('Post')} className="iconbutton"><img src={Post} alt="post"  className="icon"/></button>
+        {/* <img src={Home} alt="home"  className="icon"/>
         <img src={Search} alt="serch" className="icon"/>
-        <img src={Post} alt="post" className="icon"/>
+        <img src={Post} alt="post" className="icon"/> */}
         </div>
         )
 }
