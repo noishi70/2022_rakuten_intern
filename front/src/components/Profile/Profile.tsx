1a36ef47d6e4b4073c068db1472c918dc682a37a
noishi70 <74105563+noishi70@users.noreply.github.com>
2022-09-14 11:30:39 +0900

    change headerimg

--- front/src/components/Profile/Profile.tsx
+++ front/src/components/Profile/Profile.tsx
@@ -9,25 +9,25 @@ const Profile = () => {
         <img src={HeadImg} className="headimg" alt="Header" />
       </div>
       
-      <div className="row">
-        <div className="iconname">
-          <img src={HeadImg} className="iconimage" alt="icon" />
-          <div className="username">UserName</div>
+      <div className="bodyelem">
+        <div className="row">
+          <div className="iconname">
+            <img src={HeadImg} className="iconimage" alt="icon" />
+            <div className="username">UserName</div>
+          </div>
+          <button className="profilebutton">プロフィール編集</button>
         </div>
-        <button className="profilebutton">プロフィール編集</button>
-      </div>
-      
 
-      <div className="profile">
-        <p className="profileintro">
-          fdaffffffffffffffssssssssssssssssssssssssffffffffffafdsafsadfasf
-        </p>
-      </div>
+        <div className="profile">
+          <p className="profileintro">
+            fdaffffffffffffffssssssssssssssssssssssssffffffffffafdsafsadfasf
+          </p>
+        </div>
 
-      <div className="followfollower">
-        <div className="follow">1234 フォロー</div>
-        <div className="follower">5678 フォロワー</div>
-
+        <div className="followfollower">
+          <div className="follow">1234 フォロー</div>
+          <div className="follower">5678 フォロワー</div>
+        </div>
       </div>
 
       <hr></hr>
