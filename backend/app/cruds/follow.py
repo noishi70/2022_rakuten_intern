c87bad1329a5ca0f978302eb041d5f599209347d
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-15 10:47:36 +0900

    docs: add

--- backend/app/cruds/follow.py
+++ backend/app/cruds/follow.py
@@ -44,6 +44,11 @@ def unfollow(follower_id: str, followee_id: str):
 
 
 def get_followers(followee_id: str):
+    """フォロワーIDを取得
+
+    Args:
+        followee_id (str): 自分のID
+    """
     followers = session.query(Follow) \
         .filter(Follow.followee_id==followee_id).all()
     session.close()
@@ -51,18 +56,33 @@ def get_followers(followee_id: str):
 
 
 def get_followees(follower_id: str):
+    """フォロイーIDを取得
+
+    Args:
+        follower_id (str): 自分のID
+    """
     followees = session.query(Follow) \
         .filter(Follow.follower_id==follower_id).all()
     session.close()
     return followees
 
 def count_followers(user_id: str):
+    """フォロワーの数を取得
+
+    Args:
+        user_id (str): 自分のID
+    """
     number_of_followers = session.query(Follow) \
                     .filter(Follow.follower_id == user_id).count()
     session.close()
     return number_of_followers
 
 def count_followees(user_id: str):
+    """フォロイーの数を取得
+
+    Args:
+        user_id (str): 自分のID
+    """
     number_of_followers = session.query(Follow) \
                     .filter(Follow.follower_id == user_id).count()
     session.close()
