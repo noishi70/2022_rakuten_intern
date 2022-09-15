bd95ac8133c9c6d626c5363bcc6e851df78ab672
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-09-15 09:57:15 +0900

    refac: 環境変数から読み込み

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,10 +1,7 @@
 from typing import Optional
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
-from dotenv import load_dotenv
 import os
-
-load_dotenv("../.env")
 
 DRIVER_NAME: Optional[str] = os.environ.get("DRIVER_NAME")
 DB_USER: Optional[str] = os.environ.get("DB_USER")
@@ -14,8 +11,7 @@ DB_PORT: Optional[str] = os.environ.get("DB_PORT")
 DB_NAME: Optional[str] = os.environ.get("DB_NAME")
 SECRET_KEY: Optional[str] = os.environ.get("SECRET_KEY")
 
-# URL = f"{DRIVER_NAME}+pymysql://{DB_USER}:{DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8"
-URL = "mysql+pymysql://root:docker@db:3306/reakuten_db?charset=utf8"
+URL = f"{DRIVER_NAME}+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8"
 
 Engine = create_engine(URL, echo=True)
 session = scoped_session(
